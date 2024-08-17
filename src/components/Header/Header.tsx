d22cc473a5ddc4c4d2e6a77532ab2728e2e69c5a
Eo Soo Kim <eosookim@gmail.com>
2024-08-17 16:08:34 -0400

    creates header, styling on mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,23 @@
 import "./Header.scss";
-import logo from "../../assets/images/logo.svg";
+import logo from "../../assets/images/favicon-32x32.png";
+import moon from "../../assets/images/icon-moon.svg";
+import sun from "../../assets/images/icon-sun.svg";
+import profile from "../../assets/images/image-avatar.jpg";
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <div className="logo">
-        <img src={logo} className="logo__image"></img>
+        <img src={logo} className="logo__image" />
+      </div>
+      <div className="header__div-right">
+        <div className="mode">
+          <img src={moon} className="mode__image" />
+          {/* <img src={sun} className="mode__image" /> */}
+        </div>
+        <div className="profile">
+          <img src={profile} className="profile__image"></img>
+        </div>
       </div>
     </header>
   );
