643e2e603649d017870084f52325cf294fd84ce7
Eo Soo Kim <eosookim@gmail.com>
2024-08-20 22:56:49 -0400

    finishes styling for top bar

--- src/components/EmptyInvoiceList/EmptyInvoiceList.tsx
+++ src/components/EmptyInvoiceList/EmptyInvoiceList.tsx
@@ -1,7 +1,10 @@
 import './EmptyInvoiceList.scss'
+import emptyImage from '../../assets/images/illustration-empty.svg';
 
 export default function EmptyInvoiceList(){
     return (
-        <></>
+        <div className="emptyInvoice">
+            <img src={emptyImage}></img>
+        </div>
     )
 }
--- src/components/InvoiceHeader/InvoiceHeader.tsx
+++ src/components/InvoiceHeader/InvoiceHeader.tsx
@@ -1,20 +1,24 @@
 import "./InvoiceHeader.scss";
 import plus from "../../assets/images/icon-plus.svg";
+import arrowDown from '../../assets/images/icon-arrow-down.svg';
 
 export default function InvoiceHeader() {
   return (
     <div className="InvoiceHeader">
-      <div>
-        <h1>Invoices</h1>
-        <h2>7 Invoices</h2>
+      <div className="InvoiceHeader__headers">
+        <h1 className="InvoiceHeader__header">Invoices</h1>
+        <h2 className="InvoiceHeader__subheader">7 Invoices</h2>
       </div>
       <div className="InvoiceHeader__buttons">
-        <div className="buttons__filter">Filter</div>
+        <div className="buttons__filter">
+          <h3 className="buttons__filter--text">Filter</h3>
+          <img className="buttons__filter--arrow" src={arrowDown}></img>  
+        </div>
         <button className="buttons__new">
-          <div>
+          <div className="buttons__plus--div">
             <img className="buttons__plus" src={plus}></img>
           </div>{" "}
-          New
+          <h3 className="buttons__new--text">New</h3>
         </button>
       </div>
     </div>
