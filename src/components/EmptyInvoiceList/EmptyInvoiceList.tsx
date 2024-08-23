dd007a16027394f2378d22d8830ffc14e185a798
Eo Soo Kim <eosookim@gmail.com>
2024-08-22 20:36:28 -0400

    finishes styling for empty invoice page

--- src/components/EmptyInvoiceList/EmptyInvoiceList.tsx
+++ src/components/EmptyInvoiceList/EmptyInvoiceList.tsx
@@ -1,10 +1,17 @@
-import './EmptyInvoiceList.scss'
-import emptyImage from '../../assets/images/illustration-empty.svg';
+import "./EmptyInvoiceList.scss";
+import emptyImage from "../../assets/images/illustration-empty.svg";
 
-export default function EmptyInvoiceList(){
-    return (
-        <div className="emptyInvoice">
-            <img src={emptyImage}></img>
-        </div>
-    )
+export default function EmptyInvoiceList() {
+  return (
+    <main className="emptyInvoice">
+      <img className="emptyInvoice__image" src={emptyImage}></img>
+      <div className="emptyInvoice__text">
+        <h2 className="emptyInvoice__header">There is nothing here</h2>
+        <h3 className="emptyInvoice__subheader">
+          Create an invoice by clicking the{" "}
+          <span className="emptyInvoice--new">New</span> button and get started
+        </h3>
+      </div>
+    </main>
+  );
 }
--- src/pages/InvoicePage/InvoicePage.tsx
+++ src/pages/InvoicePage/InvoicePage.tsx
@@ -1,3 +1,4 @@
+import EmptyInvoiceList from '../../components/EmptyInvoiceList/EmptyInvoiceList'
 import InvoiceHeader from '../../components/InvoiceHeader/InvoiceHeader'
 import './InvoicePage.scss'
 
@@ -5,6 +6,7 @@ export default function InvoiceList(){
     return (
         <main>
             <InvoiceHeader />
+            <EmptyInvoiceList />
         </main>
     )
 }
