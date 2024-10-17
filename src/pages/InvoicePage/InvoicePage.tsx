ff470635771ac292274a72a6b5e329b8f1e183db
Eo Soo Kim <eosookim@gmail.com>
2024-10-16 22:34:50 -0400

    properly receives data from the back end and displays them

--- src/pages/InvoicePage/InvoicePage.tsx
+++ src/pages/InvoicePage/InvoicePage.tsx
@@ -1,23 +1,59 @@
-import EmptyInvoiceList from '../../components/EmptyInvoiceList/EmptyInvoiceList'
-import Invoice from '../../components/Invoice/Invoice'
-import InvoiceHeader from '../../components/InvoiceHeader/InvoiceHeader'
+import EmptyInvoiceList from '../../components/EmptyInvoiceList/EmptyInvoiceList';
+import Invoice from '../../components/Invoice/Invoice';
+import InvoiceHeader from '../../components/InvoiceHeader/InvoiceHeader';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './InvoicePage.scss'
+import { StringOptionsWithImporter } from 'sass';
 
 type InvoiceListProps = {
     mode: string
 }
 
+interface invoice {
+    invoiceId: string,
+    createdAt: string,
+    paymentDue: string,
+    description: string,
+    paymentTerms: number,
+    clientName: string,
+    clientEmail: string,
+    status: string,
+    senderAddress: object,
+    clientAddress: object,
+    total: number
+}
+
 export default function InvoiceList({mode}:InvoiceListProps){
-    return (
-        <main className={mode === "light" ? "invoice-list" : "invoice-list invoice-list--dark"}>
+    const [invoicesList, setInvoicesList] = useState<invoice[]>([]);
+
+    useEffect(() => {
+        const getInvoiceData = async () => {
+            const response = await axios.get('http://localhost:8080/invoices')
+            setInvoicesList(response.data);
+            console.log(response.data);
+        }
+        getInvoiceData();
+    }, []);
+
+    if (invoicesList.length > 0){
+        return (
+            <main className={mode === "light" ? "invoice-list" : "invoice-list invoice-list--dark"}>
             <InvoiceHeader mode={mode}/>
-            {/* <EmptyInvoiceList /> */}
-            <Invoice mode={mode} id={"RT3080"} name={"Jensen Huang"} date={"19 Aug 2021"} amount={1800.90} status={"paid"}/>
-            <Invoice mode={mode} id={"RT3080"} name={"Jensen Huang"} date={"19 Aug 2021"} amount={1800.90} status={"pending"}/>
-            <Invoice mode={mode} id={"RT3080"} name={"Jensen Huang"} date={"19 Aug 2021"} amount={1800.90} status={"paid"}/>
-            <Invoice mode={mode} id={"RT3080"} name={"Jensen Huang"} date={"19 Aug 2021"} amount={1800.90} status={"paid"}/>
-            <Invoice mode={mode} id={"RT3080"} name={"Jensen Huang"} date={"19 Aug 2021"} amount={1800.90} status={"pending"}/>
-            <Invoice mode={mode} id={"RT3080"} name={"Jensen Huang"} date={"19 Aug 2021"} amount={1800.90} status={"paid"}/>
+            
+            {invoicesList.map((invoice) => {
+                return (
+                    <Invoice mode={mode} id={invoice.invoiceId} name={invoice.clientName} date={invoice.createdAt} amount={invoice.total} status={invoice.status}/>
+                )
+            })}
         </main>
-    )
+        );
+    } else {
+        return (
+            <main className={mode === "light" ? "invoice-list" : "invoice-list invoice-list--dark"}>
+                <InvoiceHeader mode={mode}/>
+                {/* <EmptyInvoiceList /> */}
+            </main>
+        )
+    }
 }
